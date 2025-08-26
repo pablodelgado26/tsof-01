bf5e6d5b1dc50defc4f310d10db1141537740ac1
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-08-26 10:45:08 -0300

    feat: create subtrair test

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,16 +1,28 @@
-import { somar } from '../src/calculadora.js';
+import { somar, subtrair } from '../src/calculadora.js';
 
 describe("Calculadora", () => {
-  test("Deve somar dois números corretamente", () => {
-    //AAA
-    //Arrange (preparar)
-    const a = 2;
-    const b = 3;
+    test("Deve somar dois números corretamente", () => {
+        //AAA
+        //Arrange (preparar)
+        const a = 2;
+        const b = 3;
 
-    // Act ( Executar)
-    const resultado = somar(a, b);
+        // Act ( Executar)
+        const resultado = somar(a, b);
 
-    // Assert (Verificar)
-    expect(resultado).toBe(5);
-  });
-})
+        // Assert (Verificar)
+        expect(resultado).toBe(5);
+    });
+    test("Deve subtrair dois números corretamente", () => {
+        //AAA
+        //Arrange (preparar)
+        const a = 13;
+        const b = 3;
+
+        // Act ( Executar)
+        const resultado = subtrair(a, b);
+
+        // Assert (Verificar)
+        expect(resultado).toBe(10);
+    });
+});
